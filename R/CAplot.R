6d799e931a07a31cef76f793802eeb4bf09803d8
Luke Moraglia <55449109+LukeMoraglia@users.noreply.github.com>
2022-09-19 16:15:06 -0500

    mvp for CAplot

---
+++ R/CAplot.R
@@ -0,0 +1,410 @@
+CAplot <- function(resCA,
+                   data,
+                   DESIGN = NULL,
+                   make_design_nominal = TRUE,
+                   k = 0,
+                   graphs = 12,
+                   printGraphs = FALSE,
+                   col4I = NULL,
+                   col4J = NULL,
+                   show.TI = FALSE,
+                   show.CI = TRUE,
+                   mean.cex = 3,
+                   mean.textcex = 3,
+                   display.labels.ind = FALSE,
+                   display.labels.var = TRUE,
+                   display.points.mean = TRUE,
+                   mean.constraints = NULL,
+                   scale.mean.constraints = 1.5,
+                   max.n4bar = 40,
+                   save2pptx = FALSE,
+                   title4pptx = "CA Results"){
+
+  #Debug graphs
+  printTest <- TRUE
+
+  if (is.null(col4I)) {
+    if (is.null(DESIGN)){
+      col4I <- resCA$Plotting.Data$fi.col
+    }else{
+      col4I <- createColorVectorsByDesign(makeNominalData(as.matrix(DESIGN)))$oc
+    }
+  }
+  if (is.null(col4J)) {
+    col4J <- resCA$Plotting.Data$fj.col
+  }
+
+  if ((is.data.frame(DESIGN)|is.matrix(DESIGN)) && ncol(DESIGN) > 1){
+    DESIGN <- DESIGN %*% diag(1:ncol(DESIGN)) |> rowSums()
+  }else if((is.data.frame(DESIGN)|is.matrix(DESIGN)) && ncol(DESIGN) == 1){
+    DESIGN <- as.vector(as.matrix(DESIGN))
+  }
+
+  # Scree
+  scree <- PTCA4CATA::PlotScree(ev = resCA$ExPosition.Data$eigs,
+                                plotKaiser = TRUE)
+  a01.leScree <- recordPlot()
+
+  #_________________________________________________
+  ## The I-set (plot the rows)----
+  #_________________________________________________
+
+  # look at the help for PTCA4CATA::createFactorMap
+
+  Fi   <- resCA$ExPosition.Data$fi
+  Fj   <- resCA$ExPosition.Data$fj
+  constraintsIJ <- minmaxHelper(mat1 = Fi,
+                                mat2  = Fj)
+
+
+  jolie.ggplot1 <- PTCA4CATA::createFactorMap(
+    Fi,
+    constraints = constraintsIJ,
+    col.points = col4I,
+    col.labels = col4I,
+    display.labels = display.labels.ind,
+    font.face = 'italic'
+  )
+
+  jolie.ggplot1.transparent <- PTCA4CATA::createFactorMap(
+    Fi,
+    constraints = constraintsIJ,
+    col.points = col4I,
+    col.labels = col4I,
+    display.labels = display.labels.ind,
+    alpha.points = 0.2,
+    font.face = 'italic'
+  )
+
+  # Create the labels for Inertia per dimension
+  label4Map <- PTCA4CATA::createxyLabels.gen(1,
+                                             2,
+                                             lambda = resCA$ExPosition.Data$eigs,
+                                             tau = resCA$ExPosition.Data$t)
+  a3.JolieggMap <- jolie.ggplot1$zeMap +
+    label4Map +
+    labs(title = 'The Row Map')
+
+  if(printTest){
+    print(a3.JolieggMap)
+  }
+
+  # To make some changes at the labels:
+  #  use and modify this code
+  label4Map2 <- list(label4Map,
+                     # The standard label from createxyLabels.gen
+                     theme(
+                       axis.title = element_text(
+                         # the new theme
+                         color = "darkorchid4",
+                         size = rel(1.1),
+                         # relative to default
+                         # family = 'Times',
+                         # "Times", "sans", "Courier"
+                         face   = "italic" ,
+                         # 'plain','italic', 'bold',
+                         # NB: face does not work with current ggplot2
+                       ),
+                       # end of element_text
+                       plot.title = element_text(color = '#5826A3')
+                     ))
+
+  a4.JolieggMap <-
+    jolie.ggplot1$zeMap + label4Map2
+  if (printTest) {
+    print(a4.JolieggMap)
+  }
+
+  # Final row factor scores graph
+  a4.JolieggMap.2 <-
+    a3.JolieggMap + label4Map2
+  if(printTest){
+    print(a4.JolieggMap.2)
+  }
+
+  if (printGraphs) {
+    png('RowImap.png')
+    print(a4.JolieggMap.2)
+    dev.off()
+  }
+
+  # to look at the map with or without the means
+
+  if (!is.null(DESIGN)) {
+    fi.mean <- getMeans(resCA$ExPosition.Data$fi, DESIGN)
+    colnames(fi.mean) <- paste0("Dimension ", 1:ncol(fi.mean))
+    if (is.null(mean.constraints)) {
+      mean.constraints <- lapply(minmaxHelper(fi.mean), '*', scale.mean.constraints)
+    }
+
+    grpidx.tmp <- tapply(seq_along(col4I), col4I, identity)[unique(col4I)]
+    grpidx <- sapply(grpidx.tmp, "[[", 1)
+    col4CI <- names(grpidx)
+    names(col4CI) <- DESIGN[grpidx]
+    # reorder according to fi.mean
+    col4CI <- col4CI[rownames(fi.mean)]
+
+    plot.fi.mean <- createFactorMap(fi.mean,
+                                    col.background = NULL,
+                                    col.axes = "orchid4",
+                                    alpha.axes = 0.5,
+                                    #title = "The Observation Map.",
+                                    col.points = col4CI[rownames(fi.mean)],
+                                    col.labels =  col4CI[rownames(fi.mean)],
+                                    constraints = mean.constraints,
+                                    display.points = display.points.mean,
+                                    cex = mean.cex,
+                                    text.cex = mean.textcex,
+                                    pch = 17,
+                                    alpha.points = 0.8)
+
+    bootCI.res <- Boot4Mean(resCA$ExPosition.Data$fi, DESIGN)
+    colnames(bootCI.res$BootCube) <- paste0("Dimension ", 1:ncol(resCA$ExPosition.Data$fi))
+    fi.CI <- MakeCIEllipses(bootCI.res$BootCube,
+                            col = col4CI,
+                            alpha.ellipse = 0.1,
+                            line.size = 0.5, alpha.line = 0.2)
+
+    data4TI <- resCA$ExPosition.Data$fi
+    colnames(data4TI) <- paste0("Dimension ", 1:ncol(data4TI))
+    fi.TI <- MakeToleranceIntervals(data4TI,
+                                    design = DESIGN,
+                                    axis1 = 1, axis2 = 2,
+                                    col = col4CI,
+                                    line.size = 1,
+                                    alpha.ellipse = 0.05, alpha.line = 0.3,
+                                    p.level = .80)
+
+    a5.JolieggMap <- jolie.ggplot1.transparent$zeMap + label4Map2 + plot.fi.mean$zeMap_dots + plot.fi.mean$zeMap_text
+
+    if (show.TI) {
+      a5.JolieggMap.Fi.TI <- jolie.ggplot1.transparent$zeMap + label4Map2 + fi.TI + plot.fi.mean$zeMap_dots + plot.fi.mean$zeMap_text
+      a5.JolieggMap.Fimean.TI <- jolie.ggplot1.transparent$zeMap_background + label4Map2 + fi.TI + plot.fi.mean$zeMap_dots + plot.fi.mean$zeMap_text
+      if (printGraphs) {
+        png('ObservationImapWithTI.png')
+        print(a5.JolieggMap.Fi.TI)
+        dev.off()
+
+        png('ObservationImapWithTI(MeansOnly).png')
+        print(a5.JolieggMap.Fimean.TI)
+        dev.off()
+      }
+    }
+    if (show.CI) {
+      a5.JolieggMap.Fi.CI <- jolie.ggplot1.transparent$zeMap + label4Map2 + fi.CI + plot.fi.mean$zeMap_dots + plot.fi.mean$zeMap_text
+      a5.JolieggMap.Fimean.CI <- jolie.ggplot1.transparent$zeMap_background + label4Map2 + fi.CI + plot.fi.mean$zeMap_dots + plot.fi.mean$zeMap_text
+      if (printGraphs) {
+        png('ObservationImapWithCI.png')
+        print(a5.JolieggMap.Fi.CI)
+        dev.off()
+
+        png('ObservationImapWithCI(MeansOnly).png')
+        print(a5.JolieggMap.Fimean.CI)
+        dev.off()
+      }
+    }
+    if (show.TI == TRUE & show.CI == TRUE) {
+      a5.JolieggMap.Fi.CITI <- jolie.ggplot1.transparent$zeMap + label4Map2 + fi.TI + fi.CI + plot.fi.mean$zeMap_dots + plot.fi.mean$zeMap_text
+      a5.JolieggMap.Fimean.CITI <- jolie.ggplot1.transparent$zeMap_background + label4Map2 + fi.TI + fi.CI + plot.fi.mean$zeMap_dots + plot.fi.mean$zeMap_text
+      if (printGraphs) {
+        png('ObservationImapWithCITI.png')
+        print(a5.JolieggMap.Fi.CITI)
+        dev.off()
+
+        png('ObservationImapWithCITI(MeansOnly).png')
+        print(a5.JolieggMap.Fimean.CITI)
+        dev.off()
+      }
+    }
+
+
+
+
+  }
+
+
+
+  # Fj factor map
+
+  jolie.ggplot.J.fj <- PTCA4CATA::createFactorMap(
+    Fj,
+    constraints = constraintsIJ,
+    col.points = col4J,
+    col.labels = col4J,
+    display.labels = display.labels.var
+  )
+
+  b3.jolieggMap.J.fj <-
+    jolie.ggplot.J.fj$zeMap +
+    label4Map2 +
+    labs(title = 'The Column Map')
+  if(printTest){
+    print(b3.jolieggMap.J.fj)
+  }
+
+  joint.ggplot <- PTCA4CATA::createFactorMapIJ(
+    Fi, Fj,
+    col.points.i = col4I,
+    col.labels.i = col4I,
+    col.points.j = col4J,
+    col.labels.j = col4J,
+    constraints = constraintsIJ,
+    font.face.i = "italic")
+
+  joint.map <-
+    joint.ggplot$baseMap + joint.ggplot$I_points + joint.ggplot$I_labels +
+    joint.ggplot$J_points + joint.ggplot$J_labels + label4Map2
+
+  if(printTest){
+    print(joint.map)
+  }
+
+  ### Contributions ----
+  signed.ctrI <- resCA$ExPosition.Data$ci *
+    sign(resCA$ExPosition.Data$fi)
+  if (!is.null(DESIGN)){
+    signed.ctrI <- signed.ctrI[order(DESIGN),]
+  }
+  ### ctr4I -----
+  leStem <-  'I-Set'
+  nfac  <- 1
+  nind <- nrow(signed.ctrI)
+  if (nind < 10){
+    font.size.ctr <- 5
+  }else {
+    font.size.ctr = 5-(round((nind-10)/10)-1)
+  }
+  ctrI1 <- plotCtr(signed.ctrI, col4I,
+                   nfac, stem = leStem,
+                   font.size = font.size.ctr)
+
+  nfac  <- 2
+  ctrI2 <- plotCtr(signed.ctrI, col4I,
+                   nfac, stem = leStem,
+                   font.size = font.size.ctr)
+
+  if (nind > max.n4bar){
+    ctrI1$layers[[3]] <- NULL
+    ctrI2$layers[[3]] <- NULL
+  }
+
+  # Contributions ----
+  signed.ctrJ <- resCA$ExPosition.Data$cj *
+    sign(resCA$ExPosition.Data$fj)
+  ## plot ctr ----
+  # plot contributions for component 1
+  col4J.bar <- col4J
+  #col4J.bar[3] <- '#2F7D1F'
+  nfac  <- 1
+  nvar <- nrow(signed.ctrJ)
+  if (nvar < 10){
+    font.size.ctr <- 5
+  }else {
+    font.size.ctr = 5-(round((nvar-10)/10)-1)
+  }
+
+  ctrJ1 <-
+    plotCtr(signed.ctrJ, col4J.bar, nfac,
+            font.size = font.size.ctr)
+  nfac  <- 2
+  ctrJ2 <-
+    plotCtr(signed.ctrJ, col4J.bar, nfac,
+            font.size = font.size.ctr)
+  if (nvar > max.n4bar){
+    ctrJ1$layers[[3]] <- NULL
+    ctrJ2$layers[[3]] <- NULL
+  }
+
+
+  # return lists ----
+  ## stat ----
+  results.stats <- list(
+    ExPosition.Data = resCA$ExPosition.Data,
+    Plotting.Data = resCA$Plotting.Data
+  )
+
+  ## graphs ----
+  results.graphs <- list(
+    # cor and cov mat here
+    #covariance = a5.02.covMap,
+    #correlation = a5.02.correlationMap,
+    scree = a01.leScree,
+    ctrI.1 = ctrI1,
+    ctrI.2 = ctrI2,
+    factorScoresI12 = a4.JolieggMap.2,
+    factorScoresJ12 = b3.jolieggMap.J.fj,
+    #factorScoresJ12.arrow = b3.jolieggMap.J.fj.arrow,
+    #cosineCircle4I12 = a02.jolieggMap.I,
+    #cosineCircleJ12  =  b1.jolieggMap.J,
+    ctrJ.1 = ctrJ1,
+    ctrJ.2 = ctrJ2,
+    #cosineCircleArrowJ12  =  b2.jolieggMap.J,
+    #cosineCircleArrowIJ12 = b2.jolieggMap.IJ,
+    #loadings12 = b3.jolieggMap.J.Q,
+    #loadings12.arrow = b3.jolieggMap.J.Q.arrow #,
+    # biplot12 = e.JolieBiplot
+    biplot12 = joint.map
+  )
+
+  description.graphs <- list(
+    #covariance = "The Covariance Matrix Heat Map",
+    #correlation = "The Correlation Matrix Heat Map",
+    scree = "The Eigenvalues Scree Plot",
+    ctrI.1 = "Rows: Contributions Dimension 1",
+    ctrI.2 = "Rows: Contributions Dimension 2",
+    factorScoresI12 =  "Rows: Factor Scores 1*2",
+    factorScoresJ12 = "Columns: Factor Scores 1*2",
+    #factorScoresJ12.arrow = "Variables: Loadings as Inertia 1*2 (with arrows)",
+    #cosineCircle4I12 = "Observations: Cosine Circle 1*2",
+    #cosineCircleJ12  = "Variables: Correlation Circle 1*2",
+    ctrJ.1 = "Columns: Contributions Dimension 1",
+    ctrJ.2 = "Columns: Contributions Dimension 2",
+    #cosineCircleArrowJ12  =  "Variables: Correlation Circle 1*2 (with arrows)",
+    #cosineCircleArrowIJ12 = "Variables & Observations: Correlation Circle 1*2",
+    #loadings12 = "Variables: Loadings as Weights  1*2",
+    #loadings12.arrow = "Variables: Loadings as Weights  1*2 (with arrows)" #,
+    biplot12 = "Row and Column Biplot: Factor Scores 1*2"
+  )
+
+  if (!is.null(DESIGN)){
+    results.graphs$factorScoresI12design = a5.JolieggMap
+    description.graphs$factorScoresI12design = "Rows with design: Factor Scores 1*2"
+    if (show.TI) {
+      results.graphs$factorScoresI12design.TI = a5.JolieggMap.Fi.TI
+      results.graphs$meanfactorScoresI12design.TI = a5.JolieggMap.Fimean.TI
+      description.graphs$factorScoresI12design.TI = "Rows with tolerance intervals: Factor Scores 1*2"
+      description.graphs$meanfactorScoresI12design.TI = "Rows with tolerance intervals: Mean Factor Scores 1*2"
+    }
+    if (show.CI) {
+      results.graphs$factorScoresI12design.CI = a5.JolieggMap.Fi.CI
+      results.graphs$meanfactorScoresI12design.CI = a5.JolieggMap.Fimean.CI
+      description.graphs$factorScoresI12design.CI = "Rows with confidence intervals: Factor Scores 1*2"
+      description.graphs$meanfactorScoresI12design.CI = "Rows with confidence intervals: Mean Factor Scores 1*2"
+    }
+    if (show.TI == TRUE & show.CI == TRUE) {
+      results.graphs$factorScoresI12design.CITI = a5.JolieggMap.Fi.CITI
+      results.graphs$meanfactorScoresI12design.CITI = a5.JolieggMap.Fimean.CITI
+      description.graphs$factorScoresI12design.CITI = "Rows with CI and TI: Factor Scores 1*2"
+      description.graphs$meanfactorScoresI12design.CITI = "Rows with CI and TI: Mean Factor Scores 1*2"
+    }
+  }
+
+
+
+  results <- list(
+    results.stats = results.stats,
+    results.graphs = results.graphs,
+    description.graphs = description.graphs
+  )
+
+  if (save2pptx) {
+    saveAllGraphsInList2pptx(
+      list2Save = results.graphs,
+      titles4list2Save = description.graphs,
+      file2Save.pptx = paste0(title4pptx, ".pptx"),
+      title = title4pptx
+    )
+  }
+
+  return(results)
+}
